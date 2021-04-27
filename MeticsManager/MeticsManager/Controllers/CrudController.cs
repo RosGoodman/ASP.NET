cc2880e2232b1a5b14f08e84f31bc8e3f4c9d6ab
Pavel <pavel.official@bk.ru>
2021-04-28 00:59:32 +0300

    start working

---
+++ MeticsManager/MeticsManager/Controllers/CrudController.cs
@@ -0,0 +1,52 @@
+﻿using MeticsManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeticsManager.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private readonly ValuesHolder _holder;
+
+        public CrudController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] int temperatureC, DateTime date)
+        {
+            _holder.AddNewWeather(temperatureC, date);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery] DateTime startDate, DateTime endDate)
+        {
+            List<WeatherModel> listResults = _holder.GetWeatherList(startDate, endDate);
+            if (listResults.Count == 0) NotFound();
+            return Ok(listResults);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime dateToUpdate, [FromQuery] int newTemperatureC)
+        {
+            if (!_holder.ChangeTemperature(dateToUpdate, newTemperatureC))
+                NotFound();
+
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            _holder.DeleteData(date);
+            return Ok();
+        }
+    }
+}
---
+++ MeticsManager/MeticsManager/Controllers/ValuesHolder.cs
@@ -0,0 +1,76 @@
+﻿
+using MeticsManager.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MeticsManager
+{
+    public class ValuesHolder
+    {
+        private List<WeatherModel> _weatherDiary = new List<WeatherModel>();
+
+        /// <summary>Добавить данные за новый день.</summary>
+        /// <param name="temperatureC">Температура С.</param>
+        /// <param name="date">Дата.</param>
+        public void AddNewWeather(int temperatureC, DateTime date)
+        {
+            WeatherModel newDay = new WeatherModel(temperatureC, date);
+            if(_weatherDiary.Count != 0)
+            {
+                foreach(WeatherModel day in _weatherDiary)
+                {
+                    if(day.Date == date)
+                        throw new Exception("Данные за эту дату уже внесены.");
+                }
+            }
+
+            _weatherDiary.Add(newDay);
+        }
+
+        /// <summary>Получить список погодных данных за промежуток.</summary>
+        /// <param name="startDate">Дата начала.</param>
+        /// <param name="endDate">Дата окончания.</param>
+        /// <returns>Список с результатами поиска.</returns>
+        public List<WeatherModel> GetWeatherList(DateTime startDate, DateTime endDate)
+        {
+            List<WeatherModel> list = new List<WeatherModel>();
+            foreach(WeatherModel day in _weatherDiary)
+            {
+                if (day.Date >= startDate && day.Date <= endDate)
+                    list.Add(day);
+            }
+            return list;
+        }
+
+        /// <summary>Изменить данные в заданную дату.</summary>
+        /// <param name="dateToUpdate">Дата изменения.</param>
+        /// <param name="newTemperatureC">Новое значение температуры.</param>
+        /// <returns></returns>
+        public bool ChangeTemperature(DateTime dateToUpdate, int newTemperatureC)
+        {
+            foreach (WeatherModel day in _weatherDiary)
+            {
+                if (day.Date >= dateToUpdate)
+                {
+                    day.TemperatureC = newTemperatureC;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>Удалить данные указанной даты.</summary>
+        /// <param name="deletingDate">Удаляемая дата.</param>
+        public void DeleteData(DateTime deletingDate)
+        {
+            foreach (WeatherModel day in _weatherDiary)
+            {
+                if (day.Date == deletingDate)
+                {
+                    _weatherDiary.Remove(day);
+                    return;
+                }
+            }
+        }
+    }
+}
